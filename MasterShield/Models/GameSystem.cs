95ab2712c5fcc32ed21b3b201b4b5b78773f5f4c
pedromatta <contact@pedromatta.com>
2026-09-15 20:30:46 -0300

    feat: adds the SystemBlueprint class

--- MasterShield/Models/GameSystem.cs
+++ MasterShield/Models/GameSystem.cs
@@ -9,17 +9,3 @@ public class GameSystem
     public ICollection<Campaign> Campaigns { get; set; } = new List<Campaign>();
     public ICollection<SystemBlueprint> Blueprints { get; set; } = new List<SystemBlueprint>();
 }
-
-public class SystemBlueprint
-{
-    public Guid Id { get; set; }
-    public Guid GameSystemId { get; set; }
-
-    public string TargetEntity { get; set; } = string.Empty;
-
-    public string Name { get; set; } = string.Empty;
-
-    public Dictionary<string, object> DefaultPayload { get; set; } = new();
-
-    public GameSystem GameSystem { get; set; } = null!;
-}
