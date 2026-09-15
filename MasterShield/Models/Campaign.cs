f76446da6fef42722afcb9fd5c7bb470b72b7344
pedromatta <contact@pedromatta.com>
2026-09-15 14:58:57 -0300

    feat: adds the session subdivision

--- MasterShield/Models/Campaign.cs
+++ MasterShield/Models/Campaign.cs
@@ -13,5 +13,6 @@ public class Campaign
     public ICollection<Location> Locations { get; set; } = new List<Location>();
     public ICollection<RuleCategory> RuleCategory { get; set; } = new List<RuleCategory>();
     public ICollection<Counter> Counters { get; set; } = new List<Counter>();
+    public ICollection<Session> Sessions { get; set; } = new List<Session>();
     public ICollection<Tag> Tags { get; set; } = new List<Tag>();
 }
---
+++ MasterShield/Models/Session.cs
@@ -0,0 +1,14 @@
+namespace MasterShield.Models;
+
+public class Session
+{
+    public Guid Id { get; set; }
+    public Guid CampaignId { get; set; }
+    public int SessionNumber { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public DateTime DatePlayed { get; set; }
+    public string Log { get; set; } = string.Empty;
+
+
+    public Campaign Campaign { get; set; } = null!;
+    public ICollection<Encounter> Encounters { get; set; } = new List<Encounter>();
