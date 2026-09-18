e527dd9351a1b7a43007964c662747e37920af2a
pedromatta <contact@pedromatta.com>
2026-09-18 11:43:05 -0300

    chore: add migrations for actor overview fields and category icons

--- MasterShield/Migrations/20260918131026_ActorOverviewFields.Designer.cs
+++ MasterShield/Migrations/20260918131026_ActorOverviewFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MasterShield.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MasterShield.Migrations
 {
     [DbContext(typeof(MasterShieldContext))]
-    partial class MasterShieldContextModelSnapshot : ModelSnapshot
+    [Migration("20260918131026_ActorOverviewFields")]
+    partial class ActorOverviewFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -510,10 +513,6 @@ namespace MasterShield.Migrations
                         .HasColumnType("uuid");
 
                     b.Property<string>("Icon")
-                        .IsRequired()
-                        .HasColumnType("text");
-
-                    b.Property<string>("IconId")
                         .IsRequired()
                         .HasColumnType("text");
 
---
+++ MasterShield/Migrations/20260918143202_RuleCategoryIconId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MasterShield.Migrations
+{
+    /// <inheritdoc />
+    public partial class RuleCategoryIconId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IconId",
+                table: "RuleCategories",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconId",
+                table: "RuleCategories");
+        }
+    }
+}
